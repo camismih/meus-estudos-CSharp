678f484a59c31a4b20fea6010fc63a059d6b5f44
Camis <camila.mih@outlook.com>
2022-04-15 14:13:00 -0300

    esta é uma outra forma de fazer o código anterior, sem utilizar break

---
+++ uma-outra-forma-não-usando-break/Program.cs
@@ -0,0 +1,9 @@
+﻿for(int contadorlinha = 0; contadorlinha <10; contadorlinha ++)
+{
+    for ( int contadorcoluna = 0; contadorcoluna <= contadorlinha; contadorcoluna ++)
+    {
+Console.Write("*");
+    }
+    }
+    Console.WriteLine() ;
+        Console.ReadLine();
